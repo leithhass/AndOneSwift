66e83313ec31d126fd24e51c5b7a396290ea7e62
leithhass <leithhass@gmail.com>
2025-10-09 16:07:38 +0100

    Initial Commit

---
+++ AndOneTests/AndOneTests.swift
@@ -0,0 +1,17 @@
+//
+//  AndOneTests.swift
+//  AndOneTests
+//
+//  Created by Bridges-Mobile-dev-s01 on 9/10/2025.
+//
+
+import Testing
+@testable import AndOne
+
+struct AndOneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
